ca95cbac3feba65eea6fd7c7352372827bf81ce8
blitzingeagle <mcmorrischen@gmail.com>
2017-01-05 19:51:25 -0500

    Quick fix

--- Android/HoverApp/app/src/main/java/com/hover/morris/hoverapp/other/PreferencesContainer.java
+++ Android/HoverApp/app/src/main/java/com/hover/morris/hoverapp/other/PreferencesContainer.java
@@ -6,7 +6,7 @@ import android.view.View;
  * Created by morris on 2016-12-29.
  */
 
-public Tclass PreferencesContainer {
+public class PreferencesContainer {
 	public PreferencesContainer(View view) {
 
 	}
